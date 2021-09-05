405de50aac55e7dbd839d12694edf7fa2d70e4e6
jeremiasz-goti <jeremiasz.goti@gmail.com>
2021-09-05 23:27:53 +0200

    first room and basic box collision

---
+++ Assets/scripts/npc.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class npc : MonoBehaviour
+{
+    // Setting main variables
+
+    private Vector2 moveDelta;
+    private bool movedUp;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+
+
+        
+    }
+}
